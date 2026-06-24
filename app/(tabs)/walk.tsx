00778a379cb949d03a698a91465a8e3577365ddb
Vanessa Achleitner <s2410456001@fhooe.at>
2026-06-24 13:16:28 +0200

    Add tab navigation

---
+++ app/(tabs)/walk.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function WalkScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Walk Screen</Text>
+    </View>
+  );
+}
